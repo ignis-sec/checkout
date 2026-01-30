0670f7c93c56760c0d74880fa132f0f4f44711aa
Ata Hakçıl <ahakcil@gmail.com>
2026-01-30 19:48:53 +0300

    Update git-source-provider.ts

--- src/git-source-provider.ts
+++ src/git-source-provider.ts
@@ -256,12 +256,10 @@ export async function getSource(settings: IGitSourceSettings): Promise<void> {
 
     // Submodules
     if (settings.submodules) {
-      if (settings.submoduleSshKey){
-        await authHelper.configureSubmoduleAuth()
-      }
       // Temporarily override global config
       core.startGroup('Setting up auth for fetching submodules')
       await authHelper.configureGlobalAuth()
+      await authHelper.configureSubmoduleAuth()
       core.endGroup()
 
       // Checkout submodules
